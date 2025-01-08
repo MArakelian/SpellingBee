d7a65948f32afac5d519f93deab51b146957cca3
Mark Arakelian <61424185+MArakelian@users.noreply.github.com>
2025-01-08 11:07:39 -0500

    Refactor: add main() and rework functions.py

--- src/functions.py
+++ src/functions.py
@@ -2,26 +2,15 @@
 
 
 def initital_words(file):
-    """Read the Dictionary words into a list"""
-
-    words = []
-
+    """Read the dictionary words into a list"""
     with open(file, "r") as words_file:
-        for line in words_file:
-            words.extend(line.split())
-
-    return words
+        return [line.strip() for line in words_file]
 
 
 def eliminate_words(words_with_letters, required_letters):
     """Eliminate the words that do not contain the required letters"""
-    solutions = []
-    non_solutions = []
-
-    for word in words_with_letters:
-        if all(letter in required_letters for letter in word):
-            solutions.append(word)
-        else:
-            non_solutions.append(word)
-
-    return solutions
+    return [
+        word
+        for word in words_with_letters
+        if all(letter in required_letters for letter in word)
+    ]
--- src/spellingBee.py
+++ src/spellingBee.py
@@ -5,40 +5,40 @@ from functions import *
 #  TODO: Find a more accurate list of words
 #   The actual words used by the NYT does not
 #   include all words in the dictionary.
-#   so there will be some words provdied here
+#   so there will be some words provided here
 #   that do not work in the game.
 
 
-# Filename
-filename = "../assets/words.txt"
+def main():
+    # Filename
+    filename = "../assets/words.txt"
 
-# Get the initial list of words from our .txt file
-words = initital_words(filename)
+    # Get the initial list of words from our .txt file
+    words = initital_words(file=filename)
 
-# only include words longer than four letters
-four_letter_words = [x for x in words if len(x) >= 4]
+    # Only include words longer than four letters
+    four_letter_words = [x for x in words if len(x) >= 4]
 
-# Read in the magic letter
-magic_letter = input("What letter must be in each word?: ")
+    # Read in the magic letter
+    magic_letter = input("What letter must be in each word?: ")
 
-# eliminate words that do not contain the magic letter
-solutions = words_with_letter = [x for x in four_letter_words if magic_letter in x]
+    # Eliminate words that do not contain the magic letter
+    words_with_letter = [x for x in four_letter_words if magic_letter in x]
 
-# obtain the other letters in a list
+    # Obtain the other letters in a list
+    required_letters = [
+        input("Please input a required letter and press Enter: ") for _ in range(6)
+    ]
 
-required_letters = []
-for l in range(0, 6):
-    next_letter = input("Please input a required letter and press Enter: ")
-    required_letters += next_letter
+    # Add magic_letter to required_letters
+    required_letters.append(magic_letter)
+
+    # Eliminate words that contain letters
+    solutions = eliminate_words(words_with_letter, required_letters)
+
+    # Print our solutions
+    print(solutions)
 
 
-# Add magic_letter to required_letters
-# (so you don't filter out words with magic_letter in last loop)
-required_letters += magic_letter
-
-
-# eliminate words that contain letters
-solutions = eliminate_words(words_with_letter, required_letters)
-
-# Print our solutions
-print(solutions)
+if __name__ == "__main__":
+    main()
